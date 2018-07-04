40ce4a51fbd426ec01183615d627bd10f9163f65
Markus Chiarot <cmarkus@hotmail.ch>
2018-07-04 21:15:59 +0200

    compile scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,49 @@
 const gulp = require('gulp');
 const twig = require('gulp-twig');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCss = require('gulp-clean-css');
 
 //directories
 const dir = {
 	src: {
 		twig: 'src/templates/page/**/*.twig',
 		twigWatch: 'src/templates/**/*.twig',
+		sass: 'src/scss/*.scss',
+		sassWatch: 'src/scss/**/*.scss',
 	},
-	dist: 'dist',
+	dist: {
+		base: 'dist',
+		css: 'dist/css',
+	},
 }
 
 //workers
 const buildTwig = () => {
 	return gulp.src(dir.src.twig)
 		.pipe(twig({data: {}, extname: ''}))
-		.pipe(gulp.dest(dir.dist));
+		.pipe(gulp.dest(dir.dist.base));
+}
+const buildSass = () => {
+	return gulp.src(dir.src.sass)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+				browsers: ['defaults', 'not ie < 9']
+		}))
+		.pipe(cleanCss({compability: 'ie9'}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(dir.dist.css));
 }
 
 //watches
 const watch = () => {
 	gulp.watch(dir.src.twigWatch, buildTwig);
+	gulp.watch(dir.src.sassWatch, buildSass);
 }
 
 //tasks
-gulp.task('build', gulp.parallel(buildTwig));
+gulp.task('build', gulp.parallel(buildTwig, buildSass));
 gulp.task('watch', gulp.parallel(watch));
 gulp.task('default', gulp.series('build', 'watch'));
